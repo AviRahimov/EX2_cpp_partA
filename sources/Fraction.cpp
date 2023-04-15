78ea561cfd586b7f92c5e5642605dd04b58eeff7
Avi Rahimov <rahimovavi3@gmail.com>
2023-04-15 22:54:41 +0300

    added some functions

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -8,70 +8,137 @@ Fraction::Fraction(int m, int n) : mone(m), mechane(n){
 Fraction::Fraction(Fraction& other){
 
 }
+Fraction::~Fraction(){
+
+}
 
 // binary operators
 Fraction Fraction::operator+ (Fraction& other) const{
-
+    return Fraction(0, 0);
 }
 
 Fraction Fraction::operator- (Fraction& other) const{
-
+    return Fraction(0, 0);
 }
 
 Fraction Fraction::operator* (Fraction& other) const{
-
+    return Fraction(0, 0);
 }
 
 Fraction Fraction::operator/ (Fraction& other) const{
-
+    return Fraction(0, 0);
 }
 
 bool Fraction::operator> (Fraction& other) const{
-
+    return true;
 }
 
 bool Fraction::operator< (Fraction& other) const{
-
+    return true;
 }
 
 bool Fraction::operator>= (Fraction& other) const{
-
+    return true;
 }
 
 bool Fraction::operator<= (Fraction& other) const{
-
+    return true;
 }
 
 Fraction& Fraction::operator++(){
-
+    return *this;
 }
 
-Fraction Fraction::operator++(int dummy_flag_for_postfix_increment){
-
+const Fraction Fraction::operator++(int dummy_flag_for_postfix_increment){
+    return Fraction(0, 0);
 }
 
 Fraction& Fraction::operator--(){
-
+    return *this;
 }
 
-Fraction Fraction::operator--(int dummy_flag_for_postfix_decrement){
-
+const Fraction Fraction::operator--(int dummy_flag_for_postfix_decrement){
+    return Fraction(0, 0);
 }
 
 Fraction& Fraction::operator=(const Fraction& other){
+    return *this;
+}
 
+Fraction operator+ (float f1, const Fraction& f2){
+    return Fraction(0, 0);
+}
+
+Fraction operator- (float f1, const Fraction& f2){
+    return Fraction(0, 0);
+}
+
+Fraction operator* (float f1, const Fraction& f2){
+    return Fraction(0, 0);
+}
+
+Fraction operator/ (float f1, const Fraction& f2){
+    return Fraction(0, 0);
+}
+
+Fraction operator+ (const Fraction& f2, float f1){
+    return Fraction(0, 0);
+}
+
+Fraction operator- (const Fraction& f2, float f1){
+    return Fraction(0, 0);
+}
+
+Fraction operator* (const Fraction& f2, float f1){
+    return Fraction(0, 0);
+}
+
+Fraction operator/ (const Fraction& f2, float f1){
+    return Fraction(0, 0);
+}
+
+bool operator> (float f1, const Fraction& f2){
+    return true;
+}
+
+bool operator< (float f1, const Fraction& f2){
+    return true;
+}
+
+bool operator>= (float f1, const Fraction& f2){
+    return true;
+}
+
+bool operator<= (float f1, const Fraction& f2){
+    return true;
+}
+
+bool operator> (const Fraction& f2, float f1){
+    return true;
+}
+
+bool operator< (const Fraction& f2, float f1){
+    return true;
+}
+
+bool operator>= (const Fraction& f2, float f1){
+    return true;
+}
+
+bool operator<= (const Fraction& f2, float f1){
+    return true;
 }
 
 // friend global binary operators
 bool operator== (const Fraction& f1, const Fraction& f2){
-
+    return true;
 }
 
 // friend global IO operators
 std::ostream& operator<< (std::ostream& output, const Fraction& f){
-
+    return output;
 }
 
 std::istream& operator>> (std::istream& input, const Fraction& f){
-
+    return input;
 }
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -9,16 +9,14 @@ namespace ariel{
         int mone, mechane;
         public:
         // constructor
-        Fraction(int m, int n) : mone(m), mechane(n){};
+        Fraction(int m, int n);
         // copy constructor
         Fraction(Fraction& other);
-
+        // destructor
+        ~Fraction();
         // binary operators
         Fraction operator+ (Fraction& other) const;
         Fraction operator- (Fraction& other) const;
-        //friend Fraction& operator- (const Fraction& f1, const Fraction& f2);
-        //friend Fraction& operator* (const Fraction& f1, const Fraction& f2);
-        //friend Fraction& operator+ (const Fraction& f1, const Fraction& f2);
         Fraction operator* (Fraction& other) const;
         Fraction operator/ (Fraction& other) const;
         bool operator> (Fraction& other) const;
@@ -26,13 +24,28 @@ namespace ariel{
         bool operator>= (Fraction& other) const;
         bool operator<= (Fraction& other) const;
         Fraction& operator++();
-        Fraction operator++(int dummy_flag_for_postfix_increment);
+        const Fraction operator++(int dummy_flag_for_postfix_increment);
         Fraction& operator--();
-        Fraction operator--(int dummy_flag_for_postfix_decrement);
+        const Fraction operator--(int dummy_flag_for_postfix_decrement);
         Fraction& operator=(const Fraction& other);
         // friend global binary operators
         friend bool operator== (const Fraction& f1, const Fraction& f2);
-
+        friend Fraction operator+ (float f1, const Fraction& f2);
+        friend Fraction operator- (float f1, const Fraction& f2);
+        friend Fraction operator* (float f1, const Fraction& f2);
+        friend Fraction operator/ (float f1, const Fraction& f2);
+        friend Fraction operator+ (const Fraction& f2, float f1);
+        friend Fraction operator- (const Fraction& f2, float f1);
+        friend Fraction operator* (const Fraction& f2, float f1);
+        friend Fraction operator/ (const Fraction& f2, float f1);
+        friend bool operator> (float f1, const Fraction& f2);
+        friend bool operator< (float f1, const Fraction& f2);
+        friend bool operator>= (float f1, const Fraction& f2);
+        friend bool operator<= (float f1, const Fraction& f2);
+        friend bool operator> (const Fraction& f2, float f1);
+        friend bool operator< (const Fraction& f2, float f1);
+        friend bool operator>= (const Fraction& f2, float f1);
+        friend bool operator<= (const Fraction& f2, float f1);
         // friend global IO operators
         friend std::ostream& operator<< (std::ostream& output, const Fraction& f);
         friend std::istream& operator>> (std::istream& input, const Fraction& f);
