681b805a5166b287b7a605eafa5cb5436439a09a
Avi Rahimov <rahimovavi3@gmail.com>
2023-04-17 21:12:23 +0300

    made some tests

--- Test.cpp
+++ Test.cpp
@@ -1,4 +1,6 @@
 #include "doctest.h"
+#include <sstream>
+#include <algorithm>
 #include "sources/Fraction.hpp"
 using namespace ariel;
 
@@ -40,26 +42,65 @@ TEST_CASE("Testing the / (division) operator"){
 }
 
 TEST_CASE("Testing comparison operators (>, <, >=, <=, ==)"){
-    Fraction f1(4, 5);
+    Fraction f1(-4, 5);
     Fraction f2(2, 3);
     Fraction f3(3, 7);
     Fraction f4(0, 1);
     CHECK((f3 >= 0));
-    CHECK(f2 * f3 > 0.285);
+    CHECK(f2 * f3 >= 0.285);
+    CHECK((f1 < f4));
 }
 
 TEST_CASE("Testing I/O operators"){
-
+    // Create a Fraction object
+    Fraction f1(2, 3);
+    Fraction f2(0, 1);
+    // Create an output stringstream
+    std::ostringstream output1;
+    std::ostringstream output2;
+    // Write the Fraction object to the stringstream using operator<<
+    output1 << f1;
+    output2 << f2;
+    // Check that the output is as expected
+    CHECK(output1.str() == "2/3");
+    CHECK(output2.str() == "0");
 }
 
 TEST_CASE("Randomize numbers to check Fraction functions"){
+    // Seed the random number generator
+    srand(time(0));
 
+    // Generate 100 random fractions and perform various operations on them
+    for (int i = 0; i < 100; i++) {
+        int den, num;
+        do {
+            num = rand() % 100 - 50; // Random value in the range [-50, 49]
+            den = rand() % 99 + 1;   // Random value in the range [1, 99]
+        } while (num == den); // Generate new values if num == den
+
+        // Create Fraction objects from the random values
+        Fraction f1(num, den);
+        Fraction f2(den, num);
+
+        // Calculate the gcd of the numerator and the denominator
+        int gcd = std::__gcd(num, den);
+        // Test arithmetic operators
+        CHECK(f1 + f2 == Fraction((num * num + den * den)/gcd, (den * num)/gcd));
+        CHECK(f1 - f2 == Fraction((num * num - den * den)/gcd, (den * num)/gcd));
+        CHECK(f1 * f2 == Fraction((num * den)/gcd, (den * num)/gcd));
+        CHECK(f1 / f2 == Fraction((num * num)/gcd, (den * den)/gcd));
+
+        // Test comparison operators
+        CHECK((f1 == Fraction(num, den)));
+        CHECK((f1 < f2 || f1 > f2));
+        CHECK((f1 <= f2 || f1 >= f2));
+    }
 }
 
-//TEST_CASE("Division by zero"){
-//
-//}
-
-TEST_CASE("Checking if the = operator creates a new object"){
-
+TEST_CASE("Checking if the = operator creates a new object") {
+    Fraction a(1, 2);
+    Fraction c = a;
+    a = a-0.5;
+    CHECK((a == Fraction(0, 1)));
+    CHECK((c == Fraction(1, 2)));
 }
